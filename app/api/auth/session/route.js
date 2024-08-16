061072e434344ab179a422b2e63901964d013b45
CatKeeb <boeybin@gmail.com>
2024-08-16 19:10:37 +0800

    update session api route

--- app/api/auth/session/route.js
+++ app/api/auth/session/route.js
@@ -1,6 +1,8 @@
 import { getSessionUser } from "@/utils/getSessionUser";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request) {
   const sessionUser = await getSessionUser();
   if (sessionUser) {
